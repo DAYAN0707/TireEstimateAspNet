adeee0832d6bee06e9a7cc7d93bdc156c39e781b
DAYAN0707 <kanaishikawa99@gmail.com>
2026-08-04 17:14:45 +0900

    feat: EF Coreで見積一覧取得処理を追加

--- TireEstimateAspNet/Pages/Index.cshtml.cs
+++ TireEstimateAspNet/Pages/Index.cshtml.cs
@@ -2,9 +2,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using TireEstimateAspNet.Data; // Dbcontextのあるフォルダ
 using TireEstimateAspNet.Models; // Quoteモデルのあるフォルダ
+using Microsoft.EntityFrameworkCore; // DbContextを使用するために必要
 
 namespace TireEstimateAspNet.Pages
 {
+
+// Indexページのモデル
 public class IndexModel : PageModel
 {
         private readonly ApplicationDbContext _context;
@@ -29,14 +32,24 @@ public class IndexModel : PageModel
         public int TotalAmount { get; set; }
         public bool IsCalculated { get; set; } = false;
 
-        public void OnGet()
+        // 一覧を表示するためのプロパティ（画面に表示する見積一覧を入れておく箱）
+        public List<Quote> QuoteList { get; set; } = new();
+
+        // ページが読み込まれたときに、データベースから見積もりの一覧を取得する
+        public async Task OnGetAsync()
         {
+            QuoteList = await _context.Quotes
+                .OrderByDescending(q => q.CreatedAt)
+                .ToListAsync();
         }
 
+
+        //　見積もりを計算してデータベースに保存するためのPOSTハンドラー
         public async Task<IActionResult> OnPostAsync()
         {
             TotalAmount = UnitPrice * Quantity;
 
+            //　データベースに見積もりを保存する
             var quote = new Quote
             {
                 CustomerName = string.IsNullOrWhiteSpace(CustomerName) ? "ゲスト" : CustomerName,
@@ -47,12 +60,20 @@ public class IndexModel : PageModel
                 CreatedAt = DateTime.Now
             };
 
+            //　Quotesテーブルを見る
             _context.Quotes.Add(quote);
 
             await _context.SaveChangesAsync();
 
             IsCalculated = true;
 
+            //　データベースから最新の見積もり一覧を取得する（保存→一覧取得→画面更新）
+            QuoteList = await _context.Quotes
+                .OrderByDescending(q => q.CreatedAt)
+                .ToListAsync();
+            //　List<Quote> に変換→QuoteList に入れる
+
+            // ページを再表示する
             return Page();
         }
     }
