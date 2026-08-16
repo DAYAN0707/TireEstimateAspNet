329cbec0d8fbdc264f7c64a57eea5573abc3a2d1
DAYAN0707 <kanaishikawa99@gmail.com>
2026-08-16 10:49:51 +0900

    feat: DbContextにタイヤ在庫テーブルを追加

--- TireEstimateAspNet/Data/ApplicationDbContext.cs
+++ TireEstimateAspNet/Data/ApplicationDbContext.cs
@@ -1,15 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using TireEstimateAspNet.Models;
 
-namespace TireEstimateAspNet.Data;
-
-public class ApplicationDbContext : DbContext
+namespace TireEstimateAspNet.Data
 {
-    public ApplicationDbContext(
-        DbContextOptions<ApplicationDbContext> options)
-        : base(options)
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // 見積もり情報を格納するDbSet（見積テーブル）
+        public DbSet<Quote> Quotes { get; set; }
+
+        //　タイヤ在庫情報を格納するDbSet（タイヤ在庫テーブル）
+        public DbSet<TireInventory> TireInventories { get; set; }
     }
 
-    public DbSet<Quote> Quotes { get; set; }
 }
