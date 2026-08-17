efafd5f3a8c24de9576990d7e808100eac90aaf5
DAYAN0707 <kanaishikawa99@gmail.com>
2026-08-17 23:06:26 +0900

    feat: タイヤ在庫テーブルのマイグレーションとDB反映

---
+++ TireEstimateAspNet/Migrations/20260817140114_AddTireInventory.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TireEstimateAspNet.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTireInventory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TireInventories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TireSize = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Season = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StockQuantity = table.Column<int>(type: "int", nullable: false),
+                    UnitPrice = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TireInventories", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TireInventories");
+        }
+    }
+}
