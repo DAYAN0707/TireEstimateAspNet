979ee6eba2895d6e1de44491da1c5613ddb0f49c
DAYAN0707 <kanaishikawa99@gmail.com>
2026-08-20 22:40:37 +0900

    feat: タイヤ在庫登録機能のバックエンドを実装

--- TireEstimateAspNet/Pages/Inventory.cshtml.cs
+++ TireEstimateAspNet/Pages/Inventory.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using TireEstimateAspNet.Data;
 using TireEstimateAspNet.Models;
@@ -17,12 +18,38 @@ namespace TireEstimateAspNet.Pages
         // 在庫一覧を画面に渡すためのリスト
         public List<TireInventory> InventoryList { get; set; } = new();
 
+
+        [BindProperty]
+        public TireInventory NewInventory { get; set; } = new ();
+
+
         // ページ読み込み時に DB からタイヤサイズ順で取得
         public async Task OnGetAsync()
         {
             InventoryList = await _context.TireInventories
                 .OrderBy(q => q.TireSize)
                 .ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostAsync() {
+            if (!ModelState.IsValid)
+            {
+                // 入力値が不正な場合は、在庫一覧を再取得して画面に戻す
+                InventoryList = await _context.TireInventories
+                    .OrderBy(q => q.TireSize)
+                    .ToListAsync();
+
+                return Page();
+            }
+
+            // 入力値が有効な場合は、新しい在庫を DB に追加
+            _context.TireInventories.Add(NewInventory);
+
+            //SQL Server での自動採番を利用する場合、ID は自動的に設定されるため、ここでは設定しない
+            await _context.SaveChangesAsync();
+
+            // 保存後は在庫一覧ページにリダイレクト
+            return RedirectToPage("./Inventory");
         }
     }
 }
